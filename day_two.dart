d0bb253aa6892e416ca4407f50f86010bc2a6b10
Bayu <bramadeza@gmail.com>
2021-09-17 18:26:43 +0700

    Added new peson to list

--- day_two.dart
+++ day_two.dart
@@ -13,11 +13,13 @@ void main() {
   Person person2 = new Person(name: 'Budi', age: 24);
   Person person3 = new Person(name: 'Ayu', age: 24);
   Person person4 = new Person(name: 'Siska', age: 24);
+  Person person5 = new Person(name: 'Andini', age: 24);
   List<Person> mobileClass = [];
   mobileClass.add(person);
   mobileClass.add(person2);
   mobileClass.add(person3);
   mobileClass.add(person4);
+  mobileClass.add(person5);
   mobileClass.removeLast();
   print(mobileClass.length);
   mobileClass.forEach((person) { print(person.name);});
